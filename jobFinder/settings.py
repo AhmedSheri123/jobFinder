e9cdb442ab9078af237cd10523a8d310bdba7c8d
AhmedSheri <85445168+AhmedSheri123@users.noreply.github.com>
2024-09-12 22:48:21 +0300

    wss

--- jobFinder/settings.py
+++ jobFinder/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [BASE_DIR / 'jobFinder/static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-WS_TYPE = 'ws' #set 'ws' when ssl disabled or set 'wss' on ssl enabled
+WS_TYPE = 'wss' #set 'ws' when ssl disabled or set 'wss' on ssl enabled
 
 
 
