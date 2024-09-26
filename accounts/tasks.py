1ab0dc329e410c1cbad712adc3d579e172ecf476
AhmedSheri <85445168+AhmedSheri123@users.noreply.github.com>
2024-09-26 21:37:34 +0300

    sda

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -21,7 +21,7 @@ def send_noti_task(sender_id, title, msg, country, send_local, send_by_email, se
     noti.send_by_email = send_by_email
     noti.send_by_whatsapp = send_by_whatsapp
     noti.msgs_count = receivers.count()
-    noti = noti.save()
+    noti.save()
     receivers_ids = [user.id for user in receivers]
     send_msg_email_phone_noti(title, msg, sender_id, receivers_ids, send_local, send_by_email, send_by_whatsapp, noti)
     return True
